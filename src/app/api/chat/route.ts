efa99cec0ea11212f89f31ccd6a839c73bff3b79
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-01-05 10:54:42 +0600

    setup tools and add system promt:

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,12 +1,40 @@
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
 import { openai } from "@ai-sdk/openai";
+import z from 'zod';
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
+  const SYSTEM_PROMT= `You are an expert SQL agent that helps users to query their database using natural language.
+  You have access to following tools:
+  1. Schema tool - call this tool to get the database schema which wil help you to write sql query.
+  2. db tool - call this tool to query the database.
+
+  Rules:
+  -generate only SELECT queries (no INSERT,UPDATE,DELETE,DROP)
+  -always use the schema provided by the schema tool
+  - return valid SQLite systax
+
+  Always respond in a helpful, conversational tone while being technically accurate.
+  `
+
   const result = streamText({
     model: openai("gpt-4o"),
     messages: await convertToModelMessages(messages),
+    system:SYSTEM_PROMT,
+    tools: {
+      db: tool({
+        description: 'call this tool to query a database',
+        inputSchema: z.object({
+          query: z.string().describe('The SQL query to be ran'),
+        }),
+        execute: async ({ query }) => {
+          console.log("Query",query)
+
+          return ""
+        },
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
